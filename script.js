fe4bd641878d7d892713d6f6ecbc4975074811c1
pabloyhoscarcorrea-rgb <pablo.yhoscar.correa@escola.pr.gov.br>
2026-09-16 13:48:17 -0300

    pablo

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const botoesCurtir = document.querySelectorAll(".curtir");
+boesCurtir.forEach(function(botoaoCurtir){
+    let curtiu = false;
+    botaoCurtiu.addEventListener("click , curtir");
+function curtir() {
+    const contador = botoaCurtir.queryselector("span");
+    if(curtiu === false){
+        contador.textContent++;
+    curtiu = true;}
+    else{
+        contador.texContent--;
+        curtiu = false;
+    }
+}
+}
 
